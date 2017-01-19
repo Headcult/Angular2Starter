9a5f2a21837188a0210433bf79d34b3b0b225608
Headcult <anirudh.kothur@gmail.com>
2017-01-19 23:20:07 +0530

    Added absolute name to index.html for heroku deployment

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use("/", express.static(__dirname + '/dist'));
 app.use(express.static(root));
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html')
+  // res.sendFile('index.html')
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 if (module === require.main) {
