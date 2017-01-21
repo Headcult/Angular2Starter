76f8842d27d590e492a143edd9ea130cc28aae00
Headcult <anirudh.kothur@gmail.com>
2017-01-21 08:16:01 +0530

    added postinstall ng build for heroku

--- server.js
+++ server.js
@@ -3,15 +3,23 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8000));
 
-var root = '/dist/';
+// var root = '/dist/';
+
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
 // app.use("/", express.static(__dirname + '/dist'));
-app.use(express.static('dist'));
+// app.use(express.static('dist'));
+
+// For all GET requests, send back index.html so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 // app.get('/', function (req, res) {
-//   res.render('index.html');
+//   res.sendFile('index.html');
 // });
 
 if (module === require.main) {
