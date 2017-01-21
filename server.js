dab1bf18219a6ac70b9b44c91fee594fb345e5f0
Headcult <anirudh.kothur@gmail.com>
2017-01-21 08:33:49 +0530

    changed back the port no to 8080

--- server.js
+++ server.js
@@ -3,24 +3,13 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 8000));
-
-// var root = '/dist/';
-
-// Run the app by serving the static files in the dist directory
+app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/dist'));
-
-// app.use("/", express.static(__dirname + '/dist'));
-// app.use(express.static('dist'));
 
 // For all GET requests, send back index.html so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-// app.get('/', function (req, res) {
-//   res.sendFile('index.html');
-// });
 
 if (module === require.main) {
     // Start the server
